cd86872105c05d9ba2e8e0e3c58fcbab03e002fd
HoraBla <hb347x@gmail.com>
2016-10-24 23:35:34 -0300

    Ending master detail section
    Renaming component.html to app.component.html
    Creating class for Hero
    Creating css file for component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
-export class Hero {
-
-  constructor(public id: number, public name: string){}
-}
+const HEROES: Hero[] = [
+  new Hero(11, 'Mr. Nice'),
+  new Hero(12, 'Narco'),
+  new Hero(13, 'Bombasto'),
+  new Hero(14, 'Celeritas'),
+  new Hero(15, 'Magneta'),
+  new Hero(16, 'RubberMan'),
+  new Hero(17, 'Dynama'),
+  new Hero(18, 'Dr IQ'),
+  new Hero(19, 'Magma'),
+  new Hero(20, 'Tornado')
+];
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/component.html'
+  templateUrl: 'app/app.component.html',
+  styleUrls: ['app/app.component.css']
 })
 export class AppComponent {
+  selectedHero: Hero;
+
   title = 'Tour of Heroes';
-  hero = new Hero(1, 'Windstorm');
+  heroes = HEROES;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
