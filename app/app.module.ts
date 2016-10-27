7dd4ed9fcaf96390a8709e91e4041751425d2752
HoraBla <hb347x@gmail.com>
2016-10-26 23:57:51 -0300

    Routing Section - dashboard and single hero view
    Creating dashboard
    Refactored  single hero view component to have a route pointing to it.
    Refactoring routes to a separate module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroService } from './hero.service';
 import { HeroesComponent }   from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component'
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
-  bootstrap:    [ AppComponent ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+  ],
+  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent, DashboardComponent ],
   providers:    [ HeroService ],
+  bootstrap:    [ AppComponent ],
 })
 export class AppModule { }
