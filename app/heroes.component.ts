cf289f9e427bac956570232bb3b377c47bc3c2fc
HoraBla <hb347x@gmail.com>
2016-10-27 23:22:03 -0300

    Http Section - save, update, delete heroes
    Changing mock service to in memory db with rest api access.
    Fxing bug when referencing in-memory-web-api in systemjs.config. We need the path to the umd.js file

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -21,7 +21,9 @@ export class HeroesComponent implements OnInit {
   }
 
   private getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().then(
+      heroes => this.heroes = heroes
+    );
   }
 
   onSelect(hero: Hero): void {
@@ -31,4 +33,26 @@ export class HeroesComponent implements OnInit {
   gotoDetail(): void {
     this.router.navigate(['/detail', this.selectedHero.id]);
   }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroService
+        .delete(hero.id)
+        .then(() => {
+          this.heroes = this.heroes.filter(h => h !== hero);
+          if (this.selectedHero === hero) {
+            this.selectedHero = null;
+          }
+        });
+  }
+
 }
