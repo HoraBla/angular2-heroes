194e05301168e45501f7d94b7f31efd34cbbcc61
HoraBla <hb347x@gmail.com>
2016-10-27 22:14:50 -0300

    Routing Section - last steps
    Mostly styling changes and additions
    heroes component navigates to single hero

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -13,7 +14,7 @@ export class HeroesComponent implements OnInit {
   selectedHero: Hero;
   heroes: Hero[];
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService, private router: Router) {}
 
   ngOnInit(): void {
     this.getHeroes();
@@ -25,5 +26,9 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 }
